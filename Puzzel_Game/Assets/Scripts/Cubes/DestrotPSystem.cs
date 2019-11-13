85bce2be72eaa97dd5dba1a7ac6a9a9d5d17d694
DESKTOP-CTA26N5\Dani Steunebrink <danisteunebrink@live.nl>
2019-11-13 09:38:22 +0100

    destroyable block with particle system

---
+++ Puzzel_Game/Assets/Scripts/Cubes/DestrotPSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestrotPSystem : MonoBehaviour
+{
+
+    private ParticleSystem pSystem;
+    private float maxDuration;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pSystem = GetComponent<ParticleSystem> ();
+        maxDuration = 0.00f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        maxDuration += Time.deltaTime;
+        if (pSystem.main.duration == maxDuration) {
+            Destroy (this.gameObject);
+        }
+    }
+}
---
+++ Puzzel_Game/Assets/Scripts/Cubes/DestroyCube.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCube : MonoBehaviour
+{
+
+    public GameObject PSystem;
+
+    private void OnTriggerStay (Collider other) {
+        if (other.CompareTag("Player")) {
+            if (Input.GetKeyDown(KeyCode.E)) {
+                Instantiate (PSystem, this.transform.position, this.transform.rotation);
+                Destroy (this.gameObject);
+            }
+        }
+    }
+}
